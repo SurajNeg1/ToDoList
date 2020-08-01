70703c6bb4350cb8a454f686364d993a1e657e8d
judgesupreme <1234surajsinghnegi@gmail.com>
2020-08-01 23:27:56 +0530

    Updates

---
+++ precache-manifest.20064ec417bc58b9f93e8cda2ebf0206.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2896e546d20f0dab85fdfd6ede46070",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "829be7785bea0de709a5",
+    "url": "/ToDoList/static/css/main.434e3650.chunk.css"
+  },
+  {
+    "revision": "5db7f2d4b03ed35435c7",
+    "url": "/ToDoList/static/js/2.03b3a3f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList/static/js/2.03b3a3f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "829be7785bea0de709a5",
+    "url": "/ToDoList/static/js/main.828df449.chunk.js"
+  },
+  {
+    "revision": "b3a82003f25af9c4504c",
+    "url": "/ToDoList/static/js/runtime-main.3d9c94f0.js"
+  }
+]);
